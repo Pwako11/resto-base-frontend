97243b9533cf6db011a9031b8c3693449855bd73
Pwako11 <akolo@hotmail.com>
2022-02-15 11:50:39 -0500

    added current user, loginForm, signUpForm, newProductForm, product

--- src/store.js
+++ src/store.js
@@ -1,15 +1,19 @@
 import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 import usersReducer from './reducers/user.js';
-// import currentUser from './reducers/currentUser.js';
-// import loginForm from './reducers/loginForm.js';
-// import signUpForm from './reducers/signupForm.js';
+import currentUser from './reducers/currentUser.js';
+import loginForm from './reducers/loginForm.js';
+import signUpForm from './reducers/signupForm.js';
+import product from './reducers/product.js';
+import newProductForm from './reducers/newProductForm.js';
 
 const reducer = combineReducers({
     users: usersReducer,
-    // currentUser, 
-    // loginForm,  
-    // signUpForm,  
+    currentUser,  
+    loginForm,  
+    signUpForm,
+    newProductForm,
+    product,  
   })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
