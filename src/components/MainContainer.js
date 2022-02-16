4cf29b79c262ae228a40627e8be152cac4a6ebc0
Pwako11 <akolo@hotmail.com>
2022-02-16 07:51:03 -0500

    final edits

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-const MainContainer = ({products}) => {
+const MainContainer = () => {
 
     return (
         <div className="mainContainer">
+            <p> What would you like to do today? </p>
 
+            <p>Choose the options in the navigation bar to view your product list or add products to your list.  </p>
         </div>
     )
 }
